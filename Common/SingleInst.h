7c2252094594f680105ef8f077e1e6830785d27f
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2021-07-02 10:38:08 +0800

    add singleton

---
+++ Common/SingleInst.h
@@ -0,0 +1,50 @@
+#ifndef SINGLE_INST
+#define SINGLE_INST
+
+/*使用once_flag和call_oncel来保证线程安全*/
+#include <mutex>
+#include <memory>
+
+template <typename T>
+class SingleInst
+{
+public:
+	static T& GetInstance()
+	{
+		static std::once_flag flg;
+		std::call_once(flag, [&]() { __inst.reset(new T); });
+		return *__inst;
+	}
+
+private:
+	SingleInst() = default;
+	SingleInst(const SingleInst&) = delete;
+	SingleInst& operate = (const SingleInst&) = delete;
+
+private:
+	static std::unique_ptr<T> __inst;
+};
+
+/*
+静态局部变量初始化是线程安全的
+使用静态局部变量能延迟构造实现懒汉式单例
+
+template <typename T> 
+class SingleInst
+{
+public:
+	static T& GetInstance()
+	{		
+		static T t;
+		return t;
+	}
+
+	~SingleInst() = default;
+
+private:
+	SingleInst() = default;
+	SingleInst(const SingleInst&) = delete;
+	SingleInst& operate = (const SingleInst&) = delete;
+};
+*/
+#endif // SINGLE_INST
