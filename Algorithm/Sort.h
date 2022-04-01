bb90b3caba0bea689dd2a2fbdcedae2de057048b
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2022-04-01 16:11:43 +0800

    add merge sort

--- Algorithm/Sort.h
+++ Algorithm/Sort.h
@@ -5,7 +5,7 @@
 #include <vector>
 /*
 template <class T>
-void QuickSort(std::vector<T>& input, int32_t start, int32_t end)
+void _QuickSort(std::vector<T>& input, int32_t start, int32_t end)
 {
 	assert(start >= 0);
 	if (start >= end)
@@ -28,14 +28,14 @@ void QuickSort(std::vector<T>& input, int32_t start, int32_t end)
 	}
 	// 退出循环时，必定l和r位置重叠，此时交换l和base_index的位置，就能保证左边时小于基准数，右边时大于基准数 
 	swap(&(input[l]), &(input[base_index]));
-	QuickSort<T>(input, start, l - 1);
-	QuickSort<T>(input, l + 1, end);
+	_QuickSort<T>(input, start, l - 1);
+	_QuickSort<T>(input, l + 1, end);
 }
 
 
 // 改进swap，减少内存开销
 template <class T>
-void QuickSort(std::vector<T>& input, int32_t start, int32_t end)
+void _QuickSort(std::vector<T>& input, int32_t start, int32_t end)
 {
 	assert(start >= 0);
 	if (start >= end)
@@ -60,13 +60,13 @@ void QuickSort(std::vector<T>& input, int32_t start, int32_t end)
 	// 放base前，把l位置上的数放在base_index上保证数据完整 
 	input[base_index] = input[l];
 	input[l] = base;
-	QuickSort<T>(input, start, l - 1);
-	QuickSort<T>(input, l + 1, end);
+	_QuickSort<T>(input, start, l - 1);
+	_QuickSort<T>(input, l + 1, end);
 }
 */
 
 template <class T>
-void QuickSort(std::vector<T>& input, int32_t start, int32_t end)
+void _QuickSort(std::vector<T>& input, int32_t start, int32_t end)
 {
 	assert(start >= 0);
 	if (start >= end)
@@ -84,8 +84,48 @@ void QuickSort(std::vector<T>& input, int32_t start, int32_t end)
 		input[r] = input[l]; // r成为坑位存放l位置上的数，然后l因为数据已经保存成为坑位
 	}
 	input[l] = base; // 退出循环，l和r位置重合，这个位置是最后的坑位，放会base数，然后左边是小于base，右边是大于base 
-	QuickSort<T>(input, start, l - 1);
-	QuickSort<T>(input, l + 1, end);
+	_QuickSort<T>(input, start, l - 1);
+	_QuickSort<T>(input, l + 1, end);
+}
+
+template <class T>
+void QuickSort(std::vector<T>& input)
+{
+	_QuickSort<T>(input, 0, input.size() - 1);
+}
+
+template<typename T>
+void _MergeSortRecursive(std::vector<T>& input, std::vector<T>& temp, int start, int end)
+{
+	assert(start >= 0);
+	if (start >= end)
+		return;
+	int mid = (start + end) / 2;
+	// 将数组分割 
+	_MergeSortRecursive<T>(input, temp, start, mid);
+	_MergeSortRecursive<T>(input, temp, mid + 1, end);
+	int i = start;
+	int start1 = start, start2 = mid + 1, end1 = mid, end2 = end;
+	// 合并分割后的单元 
+	// 从左右两边不断选取小的数依次放入临时数组重 
+	while (start1 <= end1 && start2 <= end2)
+		temp[i++] = input[start1] < input[start2] ? input[start1++] : input[start2++];
+	// 退出条件是start1 > end1 || start2 > end2 
+	// 将剩余的数放入数组中 
+	while (start1 <= end1)
+		temp[i++] = input[start1++];
+	while (start2 <= end2)
+		temp[i++] = input[start2++];
+	for (i = start; i <= end; i++)
+		input[i] = temp[i];
+}
+
+template <class T>
+void MergeSort(std::vector<T>& input)
+{
+	std::vector<T> tmp;
+	tmp.resize(input.size());
+	_MergeSortRecursive(input, tmp, 0, input.size() - 1);
 }
 
 #endif//SORT_H
