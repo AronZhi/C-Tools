41e86b2edbe6c77b88e9e9ca2d3898833a78e78b
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2022-09-01 17:06:37 +0800

    update code

--- Async/BlockQueue.h
+++ Async/BlockQueue.h
@@ -3,23 +3,38 @@
 
 #include <mutex>
 #include <queue>
+#include <assert.h>
 
 template<class T>
 class BlockQueue
 {
 public:
-	void Push(T data)
+	void push(T data)
 	{
 		std::unique_lock<std::mutex> lock(__queue_mutex);
 		_queue.push(data);
 	}
 
-	T Pop()
+	T pop()
 	{
 		std::unique_lock<std::mutex> lock(__queue_mutex);
+		assert(!_queue.empty());
 		T data = _queue.front();
 		_queue.pop();
 		return data;
+	}
+
+	bool empty()
+	{
+		std::unique_lock<std::mutex> lock(__queue_mutex);
+		return _queue.empty();
+	}
+
+	T front()
+	{
+		std::unique_lock<std::mutex> lock(__queue_mutex);
+		assert(!_queue.empty());
+		return _queue.front();
 	}
 
 private:
