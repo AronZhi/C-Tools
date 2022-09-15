ff58a1b01a007a8d8e370f14737b80302b830816
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2022-09-15 16:43:18 +0800

    update code

--- String/Convert.h
+++ String/Convert.h
@@ -11,7 +11,7 @@ void ws_to_s(const std::wstring& src, std::string& ret)
 	ret = wcv.to_bytes(src);
 }
 
-bool s_to_ws(const std::string& src, std::wstring& ret)
+void s_to_ws(const std::string& src, std::wstring& ret)
 {
 	std::wstring_convert<std::codecvt_utf8<wchar_t> > wcv;
 	ret = wcv.from_bytes(src);
