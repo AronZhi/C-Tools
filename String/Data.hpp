81757d1f14d71506123aeb0ac6d13971d9059196
Aron_Zhi <azhi@nextlabs.com>
2022-11-03 14:55:32 +0800

    update code

--- String/Data.hpp
+++ String/Data.hpp
@@ -1,9 +1,23 @@
-#ifndef SPLIT_HPP
-#define SPLIT_HPP
+#ifndef DATA_HPP
+#define DATA_HPP
 
-#include <vector>
 #include <string>
+#include <locale>
+#include <codecvt>
+#include <vector>
 #include <regex>
+
+void ws2s(const std::wstring& src, std::string& ret)
+{
+	std::wstring_convert<std::codecvt_utf8<wchar_t> > wcv;
+	ret = wcv.to_bytes(src);
+}
+
+void s2ws(const std::string& src, std::wstring& ret)
+{
+	std::wstring_convert<std::codecvt_utf8<wchar_t> > wcv;
+	ret = wcv.from_bytes(src);
+}
 
 void split(const std::string& src, const std::string& delimiter, std::vector<std::string>& ret)
 {
@@ -17,4 +31,4 @@ void split(const std::string& src, const std::string& delimiter, std::vector<std
 	ret = { std::sregex_token_iterator(src.begin(), src.end(), re, -1),std::sregex_token_iterator() };
 }
 
-#endif // SPLIT_HPP
+#endif // DATA_HPP
