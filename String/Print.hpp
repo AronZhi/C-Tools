75dfa81c67c250ba6a9641889b3e7e21d7765921
Aron_Zhi <azhi@nextlabs.com>
2022-11-04 10:28:55 +0800

    fix output fail

--- String/Print.hpp
+++ String/Print.hpp
@@ -1,11 +1,13 @@
 #ifndef PRINT_HPP
 #define PRINT_HPP
 
+#include <sstream>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <list>
 
+/*
 template <class cls>
 void _wprintOne(cls param)
 {
@@ -52,13 +54,42 @@ void _wprintContainer(const std::list<T, Allocator>& lst)
 	for (auto item : lst)
 		_wprintOne<decltype(item)>(item);
 }
+*/
+
+template <class cls>
+void _wprintOne(std::wstringstream& ws, cls param)
+{
+	const char* type_name = typeid(param).name();
+	char* p = const_cast<char*>(type_name);
+	bool invalid = true;
+	static char invalid_print_type[5] = { 'c', 'l', 'a', 's', 's' };
+	for (int i = 0; i < 5; ++i)
+	{
+		if (*p != invalid_print_type[i])
+		{
+			invalid = false;
+			break;
+		}
+	}
+	if (invalid)
+		return;
+	else
+		ws << param << ' ';
+}
+
+template <>
+void _wprintOne<std::string>(std::wstringstream& ws, std::string param)
+{
+	ws << param.c_str() << L' ';
+}
 
 template <class... Args>
 int wprint(Args... args)
 {
+	std::wstringstream ws(L"");
 	int ret = sizeof...(args);
-	(void)std::initializer_list<int> {(_wprintOne<decltype(args)>(args), 0)...};
-	std::wcout << std::endl;
+	(void)std::initializer_list<int> {(_wprintOne<decltype(args)>(ws, args), 0)...};
+	wprintf(L"%s\n", ws.str().c_str());
 	return ret;
 }
 
