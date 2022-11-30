e6e997b34bd1d83ed31ecec483b79112e8dd65a7
Aron_Zhi <azhi@nextlabs.com>
2022-11-30 17:50:08 +0800

    update code

--- String/Print.hpp
+++ String/Print.hpp
@@ -8,7 +8,7 @@
 #include <list>
 
 template <class cls>
-void _wprintOne(std::wstringstream& ws, cls param)
+void _wprintOne(char splite, std::wstringstream& ws, cls param)
 {
 	const char* type_name = typeid(param).name();
 	char* p = const_cast<char*>(type_name);
@@ -25,55 +25,60 @@ void _wprintOne(std::wstringstream& ws, cls param)
 	if (invalid)
 		return;
 	else
-		ws << param << ' ';
+		ws << param << splite;
 }
 
 template <>
-inline void _wprintOne<std::string>(std::wstringstream& ws, std::string param)
+inline void _wprintOne<std::string>(char splite, std::wstringstream& ws, std::string param)
 {
-	ws << param.c_str() << L' ';
+	ws << param.c_str() << splite;
 }
 
 template <class... Args>
-int wprint(Args... args)
+void wprint(Args... args)
 {
 	std::wstringstream ws(L"");
-	int ret = sizeof...(args);
-	(void)std::initializer_list<int> {(_wprintOne<decltype(args)>(ws, args), 0)...};
+	if (sizeof...(args) > 0)
+		(void)std::initializer_list<int> {(_wprintOne<decltype(args)>(' ', ws, args), 0)...};
 	wprintf(L"%s\n", ws.str().c_str());
-	return ret;
 }
 
-template<class T, class Allocator>
-int wprint(const std::list<T, Allocator>& lst)
+template<class T, class Allocator, class... Args>
+void wprint(const std::list<T, Allocator>& lst, Args... args)
 {
 	std::wstringstream ws(L"");
-	ws << L"[ ";
-	int ret = lst.size();
-	if (ret > 0)
+	ws << '[';
+	if (lst.size() > 0)
 	{
 		for (auto item : lst)
-			_wprintOne<decltype(item)>(ws, item);
+			_wprintOne<decltype(item)>(',', ws, item);
 	}
-	ws << L"]";
+	ws << ']';
+	if (sizeof...(args) > 0)
+	{
+		ws << ' ';
+		(void)std::initializer_list<int> {(_wprintOne<decltype(args)>(' ', ws, args), 0)...};
+	}
 	wprintf(L"%s\n", ws.str().c_str());
-	return ret;
 }
 
-template<class T, class Allocator>
-int wprint(const std::vector<T, Allocator>& vec)
+template<class T, class Allocator, class... Args>
+void wprint(const std::vector<T, Allocator>& vec, Args... args)
 {
 	std::wstringstream ws(L"");
-	ws << L"[ ";
-	int ret = vec.size();
-	if (ret > 0)
+	ws << '[';
+	if (vec.size() > 0)
 	{
 		for (auto item : vec)
-			_wprintOne<decltype(item)>(ws, item);
+			_wprintOne<decltype(item)>(',', ws, item);
 	}
-	ws << L"]";
+	ws << ']';
+	if (sizeof...(args) > 0)
+	{
+		ws << ' ';
+		(void)std::initializer_list<int> {(_wprintOne<decltype(args)>(' ', ws, args), 0)...};
+	}
 	wprintf(L"%s\n", ws.str().c_str());
-	return ret;
 }
 
 #endif // PRINT_HPP
